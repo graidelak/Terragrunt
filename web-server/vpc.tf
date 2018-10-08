7e3fd7d3747123f21437381938dc3309752587d4
root <root@GABRIEL.localdomain>
2018-10-08 17:23:45 -0400

    tcp port

--- web-server/vpc.tf
+++ web-server/vpc.tf
@@ -81,7 +81,7 @@ resource "aws_security_group_rule" "web_allow_ssh_inbound" {
   type                  = "ingress"
   from_port             = "${var.ssh_port}"
   to_port               = "${var.ssh_port}"
-  protocol              = "tpc"
+  protocol              = "tcp"
   cidr_blocks           = ["0.0.0.0/0"]
   security_group_id     = "${aws_security_group.websg.id}"
   }
@@ -90,7 +90,7 @@ resource "aws_security_group_rule" "web_allow_http_inbound" {
   type                  = "ingress"
   from_port             = "${var.http_port}"
   to_port               = "${var.http_port}"
-  protocol              = "tpc"
+  protocol              = "tcp"
   cidr_blocks           = ["0.0.0.0/0"]
   security_group_id     = "${aws_security_group.websg.id}"
 }
