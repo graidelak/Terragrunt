a0148d45c8e55ef4ba384a3b20f8f9fdb5e0e3b3
root <root@GABRIEL.localdomain>
2018-10-09 09:18:38 -0400

    new

--- web-server/main.tf
+++ web-server/main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "webserver" {
    instance_type = "${var.instance_type}"
    key_name = "${var.key_name}"
    subnet_id = "${aws_subnet.public-subnet.id}"
-   vpc_security_group_ids = ["${aws_security_group.terraform.id}"]
+   vpc_security_group_ids = ["${aws_security_group.websg.id}"]
    associate_public_ip_address = true
    source_dest_check = false
 
