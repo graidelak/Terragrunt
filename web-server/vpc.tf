f7cff352a9eb556705aaa2db54128b2805a5bdb7
root <root@GABRIEL.localdomain>
2018-10-09 10:15:42 -0400

    zones

--- web-server/vpc.tf
+++ web-server/vpc.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "terra-aws" {
 resource "aws_subnet" "public-subnet" {
   vpc_id = "${aws_vpc.terra-aws.id}"
   cidr_block = "${var.public_subnet_cidr}"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-west-1a"
   map_public_ip_on_launch=true
 
   tags {
@@ -23,7 +23,7 @@ resource "aws_subnet" "public-subnet" {
 resource "aws_subnet" "private-subnet" {
   vpc_id = "${aws_vpc.terra-aws.id}"
   cidr_block = "${var.private_subnet_cidr}"
-  availability_zone = "us-east-1b"
+  availability_zone = "us-west-1b"
 
   tags {
      Name = "Test private subnet"
