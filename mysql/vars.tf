f898bf0ed80127d25e9b77b2c4dca79f646a73da
root <root@GABRIEL.localdomain>
2018-10-09 10:12:15 -0400

    vpc

--- mysql/vars.tf
+++ mysql/vars.tf
@@ -25,3 +25,22 @@ variable "master_username" {
 variable "master_password" {
   description = "The password for the master user of the DB"
 }
+
+variable "mysql_port" {
+  description = "mysql port"
+}
+
+variable "vpc_cidr" {
+description = "CIDR"
+default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+description = "public_subnet"
+default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+description = "private_subnet"
+default = "10.0.2.0/24"
+}
